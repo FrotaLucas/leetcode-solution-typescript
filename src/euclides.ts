41c7424cc4191702e9c569d04973342cfeef6440
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-23 15:43:00 +0200

    new code

--- src/euclides.ts
+++ src/euclides.ts
@@ -1,3 +1,18 @@
 
 
-console.log("Euclides module loaded");  
+console.log("Hii module loaded");  
+
+function solution(): Number {
+
+    var a: number = 10;
+
+    while(a > 0) {
+
+        a = a /2;
+    }
+
+
+    return 42;
+}
+
+solution();
