42c147d8cc4e8ce0bb88ab3db222ac2715353e2b
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-29 00:00:08 +0100

    finish euclides

--- src/euclides.ts
+++ src/euclides.ts
@@ -3,33 +3,44 @@ import { parse } from "path";
 
 console.log("Hii module loaded");  
 
-function solution(a: number): number {
+function convertToBinary(decimalNumber: number): string {
+    // 1proof
+    // let binaryNumber = Number(decimalNumber.toString(2)); 
+    // console.log(" Binary Number: " + binaryNumber);
 
+    //2logic
+    let binary: string = ""; 
+    while(decimalNumber >= 1) {
 
-    //proof
-
-    let binaryNumber = Number(a.toString(2)); 
-    console.log(" Binary Number: " + binaryNumber);
-
-    let s: string = ""; 
-    while(a >= 1) {
-
-        var b = a%2;
-        a = Math.floor(a/2); 
-        s = b + s;
-        // console.log("new value of a: " + a + " resto: " + b);
+        var binaryDigit = decimalNumber%2;
+        decimalNumber = Math.floor(decimalNumber/2); 
+        binary = binaryDigit + binary;
     }
 
-    if(s[0] == "0") {
-        s = s.split(s[0])[1];
-    }
-
-    const binary =  parseInt(s);
-
-
-
-    return binary;
-
+    let trimmedBinary : string;
+    binary[0] == "0" ? trimmedBinary = binary.split(binary[0])[1] : trimmedBinary = binary; 
+    
+    return trimmedBinary;   
+  
 }
 
-console.log(solution(125));    
+console.log(convertToBinary(825000000));    
+
+
+function countTotalZeros(binary: string): number
+{
+  
+    let stringNumber = binary.split("1");
+
+    let sequenceOfZeros = 0;
+
+   for(let part of stringNumber)
+   {
+    //    console.log(part);
+       if(part.length > sequenceOfZeros)
+         sequenceOfZeros = part.length;
+   }
+    return sequenceOfZeros;
+};
+
+console.log(countTotalZeros(convertToBinary(825000000)));   
