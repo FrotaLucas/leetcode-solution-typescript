c92e20bc292117bd0dba1f2ac19e611ab4827ced
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-08 18:36:34 +0100

    new comments

--- src/retangle-function.ts
+++ src/retangle-function.ts
@@ -2,3 +2,30 @@ function drawRetangle(s: string) : boolean
 {
     return false;
 }
+
+//stepe by step
+
+//retangle function
+// < verificações >
+
+// 1 verificar o total de comandos
+// quantos comandos tem ?
+// const points : [number, number []
+
+
+// 2 armazenar todos os pontos em coordenadas e verificar 
+// se para no mesmo ponto de partida [0,0]
+
+// const [xf, yx] = points[points.length -1]
+
+
+// 3 verificar os vertices. ver as dimensões para descobrir se fez curva.
+
+// const turns : [number,number][] = []
+
+// 4 precisa ter no total 4 turns
+
+// 5 verificar tamanho dos lados 
+
+
+// turn[0] eh mesma coisa que turn[0][0] ?
